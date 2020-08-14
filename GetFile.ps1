d061c5d98ac58632b58df3d2847e220932f045af
Joseph Leroux <41457887+JosephLerouxBook@users.noreply.github.com>
2020-08-14 15:24:30 +0200

    #1
    Ask for a file and return it

---
+++ GetFile.ps1
@@ -0,0 +1,9 @@
+﻿function get-file {
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog 
+    $FileBrowser.InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    $FileBrowser.Filter = 'Documents (*.docx) | (*.docx)'
+    $FileBrowser.Multiselect = $true
+    $null = $FileBrowser.ShowDialog()
+}
+
+$file = Get-file
